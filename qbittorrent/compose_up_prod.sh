83257009b803b614b56cb68f080d62257de898eb
Luca Faggion <luca.faggion@outlook.com>
2023-11-06 12:40:02 +0100

    Fixed qbittorrent ui

--- qbittorrent/compose_up_prod.sh
+++ qbittorrent/compose_up_prod.sh
@@ -1,1 +1,2 @@
 docker compose --env-file ../.env.prod --env-file .env.prod --env-file .env.bittorrent up -d "$@"
+./download_ui.sh
--- qbittorrent/download_ui.sh
+++ qbittorrent/download_ui.sh
@@ -1,5 +1,5 @@
 wget https://github.com/WDaan/VueTorrent/releases/download/v2.0.1/vuetorrent.zip -O ui.zip
 unzip ui.zip -d ./ui
 rm ui.zip
-docker cp ./ui qbittorrent:/ui
+docker cp ./ui/* qbittorrent:/ui/
 rm -rf ./ui
